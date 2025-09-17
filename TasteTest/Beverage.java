aa89f474409e035abbc01631f5652e3772af4d1f
FrederikSkipper <RK23KN@student.aau.dk>
2025-09-17 22:34:45 +0200

    .addition

--- TasteTest/Beverage.java
+++ TasteTest/Beverage.java
@@ -1,7 +1,29 @@
 package TasteTest;
 public class Beverage {
-    int Liters; 
+    private double Liters;
+    private String Colour; 
+    private boolean carbonDioxide;
+    public Beverage(double initialLiters, String initialColour, boolean carbonDioxide) {
+        Liters = initialLiters;
+        Colour = initialColour;
+        carbonDioxide = null;
+    }
     public static void main(){
-        Beverage Sodas = new Beverage();
+        Beverage Soda = new Beverage();
+
+    }
+    public int getLiters() {
+        return Liters
+    }
+    public static void main (String[] args) {
+        Beverage beverageInfo = new beverage(0.5,"red");
+        Soda sodaInfo = new soda()
     }
 } 
+public Soda (double initialLiters, String initialColour, boolean carbonDioxide ) {
+    private String Taste;
+    
+    Super(initialLiters, initialColour, null);
+
+}
+
