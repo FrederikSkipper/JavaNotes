a11358f2f20ab91f940d0113aa063fae3fc855ec
FrederikSkipper <RK23KN@student.aau.dk>
2025-10-16 09:37:21 +0200

    feat: Exceptions

---
+++ exceptions/example.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class example {
+    int balance = 0;
+    static void removeBalance(int balance) throws IndexOutOfBoundsException {
+        try {
+        balance = balance - 200;
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("You cant make a purchase");
+        }
+    }
+
+public static void main(String[] args) {
+    removeBalance(500); // Should throw exception.
+    }
+}
