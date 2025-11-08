293ffe656def6817a7528d7654553c299a8db0c4
FrederikSkipper <RK23KN@student.aau.dk>
2025-11-08 18:15:35 +0100

    [SimpleFactory] fix: I think i am Done. (Added parm to callFactory method.

--- creationalDesignPatterns/SimpelFactoryMethod/Dorritos.java
+++ creationalDesignPatterns/SimpelFactoryMethod/Dorritos.java
@@ -1,11 +1,9 @@
 package creationalDesignPatterns.SimpelFactoryMethod;
-import creationalDesignPatterns.SimpelFactoryMethod.SnackType;
+
 public class Dorritos implements Snack {
     private String name;
     private int price;
-    SnackType type;
   
-
     public Dorritos(String name, int price) {
         this.name = name;
         this.price = price;
@@ -17,9 +15,5 @@ public class Dorritos implements Snack {
 
     public int getPrice() {
         return this.price;
-    }
-
-    public SnackType getSnackType() {
-        return this.type;
     }
 }
--- creationalDesignPatterns/SimpelFactoryMethod/Snack.java
+++ creationalDesignPatterns/SimpelFactoryMethod/Snack.java
@@ -3,5 +3,4 @@ package creationalDesignPatterns.SimpelFactoryMethod;
 public interface Snack {
     public String getName();
     public int getPrice();
-    public SnackType getSnackType();
 }
