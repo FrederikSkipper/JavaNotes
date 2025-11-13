1ac06d73baa32501693d4074b978ac25294bde33
FrederikSkipper <RK23KN@student.aau.dk>
2025-11-13 20:32:29 +0100

    feat: Builder design pattern

--- creationalDesignPatterns/BuilderDesignPattern/Snack.java
+++ creationalDesignPatterns/BuilderDesignPattern/Snack.java
@@ -1,15 +1,70 @@
 package creationalDesignPatterns.BuilderDesignPattern;
 
-public class Snack {
-    private String name;
-    private int size;
-    private int price;
-    private String catagory;
+// Product
+class Snack {
+    private int saltAmount;
+    private int pepperAmount;
 
-    public Snack(SnackBuilder builder) {
-    this.name = builder.hasName(name);
-    this.size = builder.hasSize(size);
-    this.price = builder.hasPrice(price);
-    this.catagory = builder.hasCatagory(catagory);
+    private int wheatAmount;
+
+    public int getSaltAmount() {
+        return this.saltAmount;
     }
+
+    public int getPepperAmount() {
+        return this.pepperAmount;
+    }
+
+    public int getWheatAmount() {
+        return this.wheatAmount;
+    }
+
+    // Director
+    private Snack(SnackBuilder builder) {
+        this.pepperAmount = builder.pepperAmount;
+        this.saltAmount = builder.saltAmount;
+        this.wheatAmount = builder.wheatAmount;
+
+    }
+    // Builder
+    public static class SnackBuilder {
+        private int saltAmount;
+        private int pepperAmount;
+
+        private int wheatAmount;
+
+        public SnackBuilder(int saltAmount, int pepperAmount) {
+            this.saltAmount = saltAmount;
+            this.pepperAmount = pepperAmount;
+            }
+
+        public SnackBuilder setWheatAmount(int wheatAmount) {
+            this.wheatAmount = wheatAmount;
+            return this;
+            }
+            
+        public Snack build() {
+            return new Snack(this);
+        }
+
+    } 
+
+    // Client
+    public class Main {
+    public static void main(String[] args) {
+        // Method chaining. 
+        Snack dorritos = new Snack.SnackBuilder(10, 20).setWheatAmount(20).build();
+        Snack popcorn = new Snack.SnackBuilder(10, 20).build();
+        System.out.println(dorritos);
+        System.out.println(popcorn);
+
+        System.out.println(dorritos.getWheatAmount());
+
+        System.out.println(popcorn.getSaltAmount());
+        System.out.println(popcorn.getWheatAmount()); // default Zero, becouse it was optional and not chosen to build by client.
+
+
+    } 
+}
+
 }
