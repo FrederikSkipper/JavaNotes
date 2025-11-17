cd1b19bd85bb7b2fa8b60b602f8c7d1be27f2c44
FrederikSkipper <RK23KN@student.aau.dk>
2025-11-17 23:49:17 +0100

    feat: creational design revision, done for now.

--- structuralDesignPatterns/DecoratorDesignPattern/Main.java
+++ structuralDesignPatterns/DecoratorDesignPattern/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Tesla tesla = new TeslaModelY(); // Overriddes the getPrice and Getspecs methods from tesla interface.
         System.out.println(tesla.getPrice()+tesla.getSpecs()); // 300000 + Model Y
         
-        // 
+        
         tesla = new FsdDecorator(tesla); // Call this constrcutor on the above tesla model y. 
         System.out.println(tesla.getPrice()+tesla.getSpecs()); // 300000 + Model Y + Full self drive 
 
