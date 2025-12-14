8948f45c70ebbb4c40a27776e39a575fc4afb3c3
FrederikSkipper <RK23KN@student.aau.dk>
2025-12-14 18:15:36 +0100

    Begun MethodRefs

---
+++ methods/staticVSpublic/methodReference/MethodReference.java
@@ -0,0 +1,37 @@
+package methods.staticVSpublic.methodReference;
+
+import java.util.*;
+
+public class MethodReference {
+    public static void main(String[] args) {
+    /*------------------------------------------------------------- */
+    // Reference to static method
+    ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+
+        // before 
+        words.forEach(word -> System.out.println(word));
+
+        // after
+        words.forEach(System.out::println);
+    
+    /*------------------------------------------------------------- */
+    // Reference to an method of a specific instance
+    Person person = new Person();
+    
+    // before 
+    words.forEach(word -> person.talk(word));
+
+    // after
+    words.forEach(person::talk);
+
+    /*------------------------------------------------------------- */
+    // Reference to an method of abritary instance but specific type
+    
+
+
+
+
+    }
+}
---
+++ methods/staticVSpublic/methodReference/Person.java
@@ -0,0 +1,10 @@
+package methods.staticVSpublic.methodReference;
+
+import java.util.ArrayList;
+
+public class Person {
+    public void talk(String word) {
+        System.out.println(word);
+    }
+
+}
