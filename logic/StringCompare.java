40ffa2307c91575acb94d6b7158cdc831a333419
FrederikSkipper <RK23KN@student.aau.dk>
2026-01-01 16:33:12 +0100

    [StringCompare]

---
+++ logic/StringCompare.java
@@ -0,0 +1,21 @@
+package logic;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        
+    String one = new String("oh");
+    String two = new String("oh");
+    String three = "oh";
+    String four = "oh";
+
+    {/*one == two*/} // False (Not the same objects)
+
+    one.equals(two); // true (Checks String values)
+
+    three==four; // Compile error, but True
+
+    System.out.println(three == four); // Checks for reference equality TRUE
+    
+    System.out.println(one == two); // Checks for reference equality FALSE
+    
+}   
